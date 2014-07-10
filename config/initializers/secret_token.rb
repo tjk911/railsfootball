fb59ecfe6d57ed54399626f2f5877ee6066c0c20
Kai Teoh <kai@ink-cafe.com>
2014-07-10 18:57:42 -0500

    Update Gemfile for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsfootball::Application.config.secret_key_base = '37cf913ac37600835bc7da1def751935a190aa0fdc1401d3e4688f5cfaab10b12e1a75828aa0d78361a7075399b7da1fd8ae5806b15ae01ba7ae8ba8ddbf9b5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsfootball::Application.config.session_store :cookie_store, key: '_railsfootball_session'
