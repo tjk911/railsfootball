b4db9ba839ae760d67a182d2c06f23877d8dd527
Kai Teoh <kai@ink-cafe.com>
2014-07-11 12:15:27 -0500

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Railsfootball
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.compile = true
+    config.assets.precompile = true
   end
 end
