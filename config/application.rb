d8be4304d43494376f2b8cf6ef77a022278dc2c6
Kai Teoh <kai@ink-cafe.com>
2014-07-11 12:10:09 -0500

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Railsfootball
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.compile = true
+    congif.serve_static_assets = true
   end
 end
